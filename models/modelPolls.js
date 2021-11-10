ddc3201806d603b81b663bf1e99d66ab1cb2618b
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-11-10 17:46:26 +0100

    Klaus: MongoDB schemas added

--- models/modelPolls.js
+++ models/modelPolls.js
@@ -1,1 +1,68 @@
 // poll model
+
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+
+// Enkelkinder
+const pollAnswerSchema = new Schema(
+    {
+        "AnswerText": {
+            type: String,
+            required: true
+        },
+        "AnswerCount": {
+            type: Number,
+            default: 0,
+            required: true
+        },
+        "nextPollId": {
+            type: String
+        }
+    }
+);
+
+// Kinder
+const pollSchema = new Schema(
+    {
+        id: {
+            type: String,
+            default: uuidv4(),
+            required: true
+        },
+        Category: {
+            type: String,
+            required: true
+        },
+        PollQuestion: {
+            type: String,
+            required: true
+        },
+        PollAnswers: [{ pollAnswerSchema, required }],
+        nextPollId: {
+            type: String,
+            required: false
+        },
+    }
+);
+
+// Eltern
+const pollCollectionSchema = new Schema(
+    {
+        Owner: {
+            UID: {
+                type: String,
+                required: true
+            }
+        },
+        // Array of subdocuments
+        Polls: [pollSchema],
+        // Single nested subdocuments. Caveat: single nested subdocs only work
+        // in mongoose >= 4.2.0
+        Poll: pollSchema
+    }, { timestamps: true }
+);
+
+const Polls = mongoose.model('Polls', pollCollectionSchema);
+
+module.exports = Polls;
+
--- models/modelUsers.js
+++ models/modelUsers.js
@@ -1,1 +1,28 @@
 // user model
+
+const mongoose = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        UID: {
+            type: String,
+            required: true
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+            required: true
+        },
+        isOnline: {
+            type: Boolean,
+            default: false,
+            required: true
+        },
+    }, { timestamps: true }
+);
+
+const User = mongoose.model('Users', userSchema);
+
+module.exports = User;
+
+
